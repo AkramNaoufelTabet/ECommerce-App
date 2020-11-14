55e84f1cee52069b2f6302413b49c7f649efd980
akram <a.thabet@esi-sba.dz>
2020-11-14 16:29:38 +0100

    Test

---
+++ lib/ecommerce_firstClass.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-commerce app ',
+      home: Homescreen()
+    );
+    throw UnimplementedError();
+  }
+
+}
+
+class Homescreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return Homescreenstate();
+    throw UnimplementedError();
+  }
+}
+
+class Homescreenstate extends State<Homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      );
+    throw UnimplementedError();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
